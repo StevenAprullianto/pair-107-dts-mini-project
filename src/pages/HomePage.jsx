71b879fa458a6e92dcd43f6df49c4a2424ce171d
Steven Aprullianto <stevenaprullianto@gmail.com>
2022-07-21 22:21:39 +0700

    feat: homePage and loginPage new

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import ProfileNavbar from '../components/ProfileNavbar';
+
 import { Box, Button, Typography } from "@mui/material";
 import {Link} from "react-router-dom";
 
 const HomePage = () => {
     return (
         <>
+        <ProfileNavbar/> 
             <Box sx={{width:"100%", textAlign:"center"}}>
                 <Typography variant="h1" color="white">Ini Home Page :)</Typography>
 
